06341f25cfccc2eea19f51aae62d8502b8e2e437
Geocake <geoff213454@gmail.com>
2023-01-23 11:24:25 -0500

    q

--- machine.py
+++ machine.py
@@ -14,10 +14,11 @@ from sklearn.linear_model import Lasso
 from sklearn.linear_model import RidgeCV
 from sklearn.linear_model import LassoCV
 
+from sklearn.model_selection import cross_val_score 
+
 og = pd.read_csv('data.csv')
 df = og.drop('ID', axis=1)
 df = df.replace('#N/A', np.NaN)
-
 
 names = list(df.columns)
 names.remove('cur_mkt_cap()')
@@ -34,7 +35,7 @@ def predict(reg, df=df):
     table = []
     for i in X.index.values:
         ticker = og.loc[i]['ID']
-        actual = df.loc[i]['cur_mkt_cap()']
+        actual = og.loc[i]['cur_mkt_cap()']
         predicted = predictions[list(X.index.values).index(i)]
         table.append([ticker, actual, predicted, predicted/actual])
     summary = pd.DataFrame(table, columns = ['Ticker', 'Actual', 'Predicted', 'Ratio'])
@@ -191,65 +192,52 @@ def lasso_ridge_cv_pipeline(reg_type, alpha_space):
     print("R^2 (testing):", pipeline.score(X_test, y_test))
     plot_coefs(pipeline['regressor'])
     predict(pipeline)
-'''
-def correlation_matrix():
-    new_df = df.astype(float)
-    correlation_matrix = new_df.corr()
-    pd.set_option('display.max_rows', 5000)
-    pd.set_option('display.max_columns', 5000)
-    correlation_matrix.to_csv('Past Data Only 3/correlation.csv')
-
-def lasso_ridge_pipeline_evil(reg_type, alpha_space):
-    X = df.drop('cur_mkt_cap', axis=1)
-    y = df['cur_mkt_cap'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-
-    train_scores = np.empty(len(alpha_space))
-    test_scores = np.empty(len(alpha_space))
-
-    for i, a in enumerate(alpha_space):
-        if reg_type == 'ridge':
-            reg = Ridge(alpha = a)
-        else:
-            reg = Lasso(alpha = a)
-        steps = [('imputation', SimpleImputer(missing_values=np.nan, strategy='median')),
-                ('scaler', StandardScaler()),
-                ('regressor', reg)]
-        pipeline = Pipeline(steps)
-        pipeline.fit(X_train, y_train)
-        train_scores[i] = pipeline.score(X_train, y_train)
-        test_scores[i] = pipeline.score(X_test, y_test)
-
-    display_plot(train_scores, alpha_space)
-    display_plot(test_scores, alpha_space)
-    print("Best alpha:", alpha_space[np.argmax(test_scores)])
-
-    if reg_type == 'ridge':
-        reg = Ridge(alpha = alpha_space[np.argmax(test_scores)])
-    else:
-        reg = Lasso(alpha=alpha_space[np.argmax(test_scores)])
-    steps = [('imputation', SimpleImputer(missing_values=np.nan, strategy='median')),
-            ('scaler', StandardScaler()),
-            ('regressor', reg)]
-    pipeline = Pipeline(steps)
-    pipeline.fit(X, y)
-    print("R^2:", pipeline.score(X, y))
-    plot_coefs(pipeline['regressor'])
-    return pipeline
-
-
-    '''
 
 #linear()
 #linear_pipeline()
 #lasso_ridge('ridge', np.logspace(15, 25, 100))
 #lasso_ridge('lasso', np.logspace(15, 25, 100))
-lasso_ridge_pipeline('ridge', np.logspace(-5, 5, 100))
+#lasso_ridge_pipeline('ridge', np.logspace(-5, 5, 100))
 #lasso_ridge_pipeline('lasso', np.logspace(5, 15, 100))
 #lasso_ridge_cv('ridge', np.logspace(15, 25, 100))
 #lasso_ridge_cv('lasso', np.logspace(15, 25, 100))
-lasso_ridge_cv_pipeline('ridge', np.logspace(-5, 5, 100))
+#lasso_ridge_cv_pipeline('ridge', np.logspace(-5, 5, 100))
 #lasso_ridge_cv_pipeline('lasso', np.logspace(5, 15, 100))
 
-#reg = lasso_ridge_pipeline_evil('ridge', np.logspace(-5, 5, 100))
-#correlation_matrix()
+def linear_loocv():
+    new_df = df.dropna()
+    table = []
+    for index, row in new_df.iterrows():
+        train_df = new_df.drop(index)
+        test_df = new_df.loc[index:index+1]
+        X_train = train_df.drop('cur_mkt_cap()', axis=1)
+        y_train = train_df['cur_mkt_cap()'].values
+        X_test = test_df.drop('cur_mkt_cap()', axis=1)
+        y_test = test_df['cur_mkt_cap()'].values
+        reg = LinearRegression()
+        reg.fit(X_train, y_train)
+        prediction = reg.predict(X_test)
+        table.append([og.loc[index]['ID'], y_test[0], prediction[0], prediction[0]/y_test[0]])
+    summary = pd.DataFrame(table, columns = ['Ticker', 'Actual', 'Predicted', 'Ratio'])
+    summary.to_csv('predictions.csv', index = False)
+
+def linear_pipeline_loocv():
+    table = []
+    for index, row in df.iterrows():
+        train_df = df.drop(index)
+        test_df = df.loc[index:index+1]
+        X_train = train_df.drop('cur_mkt_cap()', axis=1)
+        y_train = train_df['cur_mkt_cap()'].values
+        X_test = test_df.drop('cur_mkt_cap()', axis=1)
+        y_test = test_df['cur_mkt_cap()'].values
+        steps = [('imputation', SimpleImputer(missing_values=np.nan, strategy='median')),
+            ('scaler', StandardScaler()),
+            ('regressor', LinearRegression())]
+        pipeline = Pipeline(steps)
+        pipeline.fit(X_train, y_train)
+        prediction = pipeline.predict(X_test)
+        table.append([og.loc[index]['ID'], y_test[0], prediction[0], prediction[0]/y_test[0]])
+    summary = pd.DataFrame(table, columns = ['Ticker', 'Actual', 'Predicted', 'Ratio'])
+    summary.to_csv('predictions.csv', index = False)
+
+linear_pipeline_loocv()
